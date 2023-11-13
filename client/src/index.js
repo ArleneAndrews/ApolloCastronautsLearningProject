15baffed9c0fcba4ca902f49b25abf8dc607b2c5
AAndrews <aandrews@accesshealth.services>
2023-11-13 15:45:54 -0600

    Closer to working WIP

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,12 @@ import GlobalStyles from './styles';
 import Pages from './pages';
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+
 ReactDOM.render(
   <ApolloProvider client={client}>
     <GlobalStyles />
@@ -12,7 +18,3 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-const client = new ApolloClient({
-  uri: "http://localhost:4000",
-  cache: new InMemoryCache(),
-});
