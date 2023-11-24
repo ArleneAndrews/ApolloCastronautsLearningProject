5c3b286fcea5416b7f80cd6ec4a1da47e3a1be35
AAndrews <aandrews@accesshealth.services>
2023-11-24 12:35:34 -0600

    Lift off Part 2 code and repo added

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,17 +1,13 @@
-import React from 'react';
+import React from "react";
 import { useQuery, gql } from "@apollo/client";
-import TrackCard from "../containers/track-card";
 import QueryResult from "../components/query-result";
-import { Layout } from '../components';
+import TrackCard from "../containers/track-card";
+import { Layout } from "../components";
 
-/**
- * Tracks Page is the Catstronauts home page.
- * We display a grid of tracks fetched with useQuery with the TRACKS query
- */
-/** TRACKS query to retrieve all tracks */
+ 
+/** TRACKS gql query to retrieve all tracks */
 const TRACKS = gql`
   query GetTracks {
-    
     tracksForHome {
       id
       title
@@ -26,18 +22,23 @@ const TRACKS = gql`
     }
   }
 `;
-
+ 
+/**
+* Tracks Page is the Catstronauts home page.
+* We display a grid of tracks fetched with useQuery with the TRACKS query
+*/
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
-
-  <Layout grid>
-    <QueryResult error={error} loading={loading} data={data}>
-  {data?.tracksForHome?.map((track) => (
-    <TrackCard key={track.id} track={track} />
-  ))}
-</QueryResult>
- 
-</Layout>
+   
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
-
+ 
 export default Tracks;
